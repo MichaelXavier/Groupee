91a3b9bfb06f1a66b97a3499e582924cde6ae205
Michael Xavier <michael@michaelxavier.net>
2010-05-23 23:57:27 -0700

    Add classmate hook

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,8 +4,37 @@ class Course < ActiveRecord::Base
   validates_uniqueness_of :sln
   validates_inclusion_of :quarter, :in => %w[ winter spring summer autumn ]
 
-  has_and_belongs_to_many :students, :class_name => "Users"
+  has_and_belongs_to_many :students, :class_name => "User"
   has_many :assignments, :dependent => :destroy
   has_many :links, :as => :context
   belongs_to :instructor
+
+  def add_student(student)
+    return nil if student_exists?(student)
+    self.students << student
+    student_add_hook(student)
+    self.save
+  end
+
+  def student_exists?(student)
+    begin
+      self.students.find(student.id)
+    rescue ActiveRecord::RecordNotFound
+      return false
+    end
+  end
+
+private
+  def student_add_hook(new_student)
+    link_type = LinkType.classmate
+    self.students.each do |student|
+      next if student == new_student
+      Link.create!(
+        :left_user => new_student, 
+        :right_user => student, 
+        :link_type => link_type,
+        :context => self
+      )
+    end
+  end
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe Course do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#add_student" do
+    subject {Factory.create(:course) }
+
+    before(:each) do
+      @user = Factory.create(:user)
+      @link_type = mock_model(LinkType)
+      LinkType.stub(:classmate).and_return(@link_type)
+    end
+
+    it "adds a basic member" do
+      subject.add_student(@user)
+      subject.students.count.should == 1
+    end
+
+    it "does not add existing members" do
+      subject.add_student(@user)
+      subject.add_student(@user).should == nil
+      subject.students.count.should == 1
+    end
+
+    it "does not create a link for existing members" do
+      Link.should_not_receive(:create!)
+      subject.add_student(@user)
+    end
+
+    it "creates a link for each user when a new one is added" do
+      user2 = Factory.create(:user)
+      Link.should_receive(:create!).with(:left_user => user2, :right_user => @user, :link_type => @link_type, :context => subject)
+      subject.add_student(@user)
+      subject.add_student(user2)
+    end
+  end
 end
