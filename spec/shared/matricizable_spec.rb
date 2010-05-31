7ac981982adf812b8194eb1384795e0891e245b0
Michael Xavier <michael@michaelxavier.net>
2010-05-30 19:21:05 -0700

    Matricize now sufficiently specd

--- spec/shared/matricizable_spec.rb
+++ spec/shared/matricizable_spec.rb
@@ -33,12 +33,52 @@ describe Matricizable do
       subject.matricize_with_cols {}.should == @workbook
     end
   end
-
-  pending "Do more tests of matricizable"
 end
 
 describe Matricizable::MatrixBuilder do
-  subject { Matricizable::MatrixBuilder.new }
-  pending "Do more tests of matrix builder"
+  before(:each) do
+    @sheet = mock(Spreadsheet::Worksheet).as_null_object
+    @row_map = {1 => 1, 2 => 2}
+  end
+
+  subject { Matricizable::MatrixBuilder.new(@sheet, @row_map) }
+
+  it "has accessors for the label map and sheet" do
+    subject.sheet.should == @sheet
+    subject.map.should == @row_map
+    subject.should respond_to(:sheet=)
+    subject.should respond_to(:map=)
+  end
+
+  describe "prepare_labels" do
+    it "puts the same labels in the same order both the rows and cols" do
+      @sheet.should_receive(:[]=).with(0, 0, 'User1').once
+      @sheet.should_receive(:[]=).with(0, 0, 'User1').once
+      @sheet.should_receive(:[]=).with(0, 1, 'User2').once
+      @sheet.should_receive(:[]=).with(1, 0, 'User2').once
+      subject.prepare_labels(['User1', 'User2'])
+    end
+  end
+
+  describe "add_weight" do
+    before(:each) do
+      @sheet.stub_chain(:[], :to_f => 1.5)
+    end
+
+
+    it "adds the weight to both left and right participants" do
+      @sheet.stub_chain(:[], :to_f => 2.0)
+      @sheet.should_receive(:[]=).with(1, 2, 3.5).once
+      @sheet.should_receive(:[]=).with(2, 1, 3.5).once
+      subject.add_weight(1, 2, 1.5)
+    end
+
+    it "adds a weight string of 0.0 if no weight given" do
+      @sheet.should_receive(:[]=).with(1, 2, 1.5).once
+      @sheet.should_receive(:[]=).with(2, 1, 1.5).once
+      subject.add_weight(1, 2)
+    end
+  end
+
 end
 
