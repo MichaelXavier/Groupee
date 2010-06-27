d0cfc52965a7a2356c36bd89dc546fb16a94f873
Michael Xavier <michael@michaelxavier.net>
2010-06-27 16:43:55 -0700

    Just use defaults

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,13 +7,14 @@ Spork.prefork do
   ENV["RAILS_ENV"] ||= 'test'
   require File.dirname(__FILE__) + "/../config/environment" unless defined?(Rails)
   require 'rspec/rails'
+  require 'rspec/autorun'
 
   # Requires supporting files with custom matchers and macros, etc,
   # in ./support/ and its subdirectories.
   Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
   Rspec.configure do |config|
-     config.mock_with :rspec
+    config.mock_with :rspec
   end
 end
 
